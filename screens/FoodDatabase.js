12309d1bdadbd200d89cb0a565e8801a942daa9b
demassiw <william.demassias@edu.esiee.fr>
2023-06-20 13:44:36 +0200

    appel api update list + details

--- screens/FoodDatabase.js
+++ screens/FoodDatabase.js
@@ -1,17 +1,28 @@
-import React , { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
 import FoodDatabaseService from '../services/FoodDatabaseService';
 
 export default function FoodDatabase() {
   const [foodName, setFoodName] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [selectedFood, setSelectedFood] = useState("");
+  const [details, setDetails] = useState(null);
 
   const handleFoodNameChange = (foodName) => {
     setFoodName(foodName);
   };
 
   const handleSubmit = async () => {
-    await FoodDatabaseService.searchFoodData(foodName);
-  }
+    const data = await FoodDatabaseService.searchFoodData(foodName);
+    setSuggestions(data);
+    setDetails(null);
+  };
+
+  const handleSelectedFood = async (selectedFood) => {
+    setSelectedFood(selectedFood);
+    const details = await FoodDatabaseService.searchFoodDetails(selectedFood);
+    setDetails(details);
+  };
 
   return (
     <View>
@@ -22,9 +33,28 @@ export default function FoodDatabase() {
         value={foodName}
       />
       <Button title="Submit" onPress={handleSubmit} disabled={!foodName} />
+      {/* si details alors on affiche les détails si non alors on laisse la liste de résultat de recherche */}
+      {details ? (
+        <View>
+          <Text>{selectedFood} Details:</Text>
+          <Text>Carbohydrates: {details.CHOCDF}g</Text>
+          <Text>Energy: {details.ENERC_KCAL}kcal</Text>
+          <Text>Total lipid: {details.FAT}g</Text>
+          <Text>Fiber, total dietary: {details.FIBTG}g</Text>
+          <Text>Protein: {details.PROCNT}g</Text>
+        </View>
+      ) : (
+        <View>
+          {suggestions.map((suggestion, index) => (
+            <View key={index} style={styles.suggestionItem}>
+              <Button title={suggestion} onPress={() => handleSelectedFood(suggestion)} />
+            </View>
+          ))}
+        </View>
+      )}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   input: {
@@ -35,5 +65,8 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginBottom: 10,
     paddingHorizontal: 10,
+  },
+  suggestionItem: {
+    marginBottom: 5,
   },
 });
